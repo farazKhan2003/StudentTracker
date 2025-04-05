f78af7c8ab7db57bda18ac056daa67ba93984aa9
farazKhan2003 <kfaraz@hotmail.co.uk>
2025-04-05 23:51:51 +0100

    Created submit button with dynamic colour shading as detailed in the materialUI documentation of palettes

--- studenttrackerfrontend/src/components/Student.js
+++ studenttrackerfrontend/src/components/Student.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { TextField, Container, Paper, Box } from '@mui/material';
+import { TextField, Container, Paper, Box, Button, ThemeProvider } from '@mui/material';
+import { createTheme, alpha, getContrastRatio } from '@mui/material/styles';
 
 export default function Student() {
   const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
@@ -8,7 +9,23 @@ export default function Student() {
   const [email, setEmail] = useState('');
   const [students, setStudents] = useState([]);
 
+  const redBase = '#d32f2f'
+  const redMain = alpha(redBase, 0.7);
+  const theme = createTheme({
+    palette: {
+      red: {
+        main: redMain,
+        light: alpha(redBase, 0.5),
+        dark: alpha(redBase, 0.9),
+        contrastText: getContrastRatio(redMain, '#fff') > 4.5 ? '#fff' : '#111',
+      },
+    },
+  });
+  
+  
+
   return (
+    <ThemeProvider theme={theme}>
     <Container>
       <Paper elevation={3} style={paperStyle}>
           <h1 style={{color:"LightSlateGrey"}}><u>Add Student:</u></h1>
@@ -36,11 +53,17 @@ export default function Student() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <Button variant="contained" color="red">
+  Submit 
+</Button>
         </Box>
+
+
         {name}
         {'\n'}
         {email}
       </Paper>
     </Container>
+    </ThemeProvider>
   );
 }
