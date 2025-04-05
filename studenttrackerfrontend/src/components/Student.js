512862c4ec8c2d90a8315b2032a9f027865a661e
farazKhan2003 <kfaraz@hotmail.co.uk>
2025-04-05 23:10:46 +0100

    Created fields for name and email using templates from MaterialUI

---
+++ studenttrackerfrontend/src/components/Student.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { TextField, Container, Paper, Box } from '@mui/material';
+
+export default function Student() {
+  const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [students, setStudents] = useState([]);
+
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="student-name"
+            label="Name"
+            variant="outlined"
+            
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="student-email"
+            label="Email"
+            variant="outlined"
+            
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </Box>
+      </Paper>
+    </Container>
+  );
+}
